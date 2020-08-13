c0f2d45ad75741a0fdc0c8fa414621c88cb5c5d0
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-08-13 21:01:28 +0500

    Create UserController.java

---
+++ src/main/java/com/eclat/solutions/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.eclat.solutions.controller;
+
+import com.eclat.solutions.dto.UserDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @GetMapping("v1/users")
+    public List<UserDto> findAll(){
+        return List.of(new UserDto("super@eclat.co","super_user"),
+                new UserDto("user@eclat.co","u_user"));
+    }
+}
