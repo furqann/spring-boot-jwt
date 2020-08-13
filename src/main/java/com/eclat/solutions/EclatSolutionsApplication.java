bee121e2d625974eb406e04540a1f96651e829af
furqann <furqanahmad21@hotmail.com>
2020-08-13 17:59:31 +0500

    made some changes to make project work

--- src/main/java/com/eclat/solutions/EclatSolutionsApplication.java
+++ src/main/java/com/eclat/solutions/EclatSolutionsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EclatSolutionsApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(EclatSolutionsApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(EclatSolutionsApplication.class, args);
+	}
 
 }
