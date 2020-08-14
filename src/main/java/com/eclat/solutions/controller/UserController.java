eb0da66b11c49e82e06020fb16da704ccfacdeea
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-08-14 21:07:12 +0500

    Update UserController.java

--- src/main/java/com/eclat/solutions/controller/UserController.java
+++ src/main/java/com/eclat/solutions/controller/UserController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    @GetMapping("v1/users")
+    @GetMapping("v1/users")//Should be accessible by Admin
     public List<UserDto> findAll(){
         return List.of(new UserDto("super@eclat.co","super_user"),
                 new UserDto("user@eclat.co","u_user"));
