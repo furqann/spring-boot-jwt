94e36eb18c8e4fc689c899e89fe42c0a2688207c
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-08-14 21:07:39 +0500

    Create SuperUserDetailsService.java

---
+++ src/main/java/com/eclat/solutions/configuration/SuperUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.eclat.solutions.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class SuperUserDetailsService implements UserDetailsService {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("foo", passwordEncoder.encode("foo"), new ArrayList<>());
+    }
+}
