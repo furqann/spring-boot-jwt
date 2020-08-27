70939ddcbf363382225f115689d9bb5409aa984e
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-08-27 20:37:18 +0500

    Added @Secured annotation on the UserController v1/users endpoint

--- src/main/java/com/eclat/solutions/controller/UserController.java
+++ src/main/java/com/eclat/solutions/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.eclat.solutions.controller;
 
 import com.eclat.solutions.dto.UserDto;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api")
 public class UserController {
+    @Secured("ROLE_ADMIN")
     @GetMapping("v1/users")//Should be accessible by Admin
     public List<UserDto> findAll(){
         return List.of(new UserDto("super@eclat.co","super_user"),
