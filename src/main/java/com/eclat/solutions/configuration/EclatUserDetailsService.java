00f0cf9d2c859a6f7de18902a62b741888d2a06d
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-08-27 20:36:10 +0500

    Updated the eclat user to have a SimpleGrantedAuthority

--- src/main/java/com/eclat/solutions/configuration/EclatUserDetailsService.java
+++ src/main/java/com/eclat/solutions/configuration/EclatUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.eclat.solutions.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EclatUserDetailsService implements UserDetailsService {
@@ -17,6 +19,7 @@ public class EclatUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new EclatUser("foo", passwordEncoder.encode("foo"), new ArrayList<>());
+        return new EclatUser("foo", passwordEncoder.encode("foo"),
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
     }
 }
