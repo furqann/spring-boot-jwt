914e4839b26731f08845a81c3dc3967a264c4c20
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-11-01 18:25:17 +0500

    #8 Created a shell script
    To run it on pc you need to install a git bash and then use the following command sh run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+# Step 1
+echo "Running maven clean install"
+mvn clean install
+
+# Step 2
+echo "Stopping and deleting container and image"
+docker stop eclat-service
+docker container rm eclat-service
+docker rmi eclat
+
+# Step 3
+echo "Building a new image and running a container"
+docker build -t eclat:latest . # Image creation
+docker run -d -p 8881:8881 --name eclat-service eclat # Running created image in a container
