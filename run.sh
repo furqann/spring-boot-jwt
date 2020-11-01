bb712b75c8d49b68f0085057b2fc264f3bcfc2a4
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-11-01 19:13:26 +0500

    #8 Updated the shell script to run the docker-compose file

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ docker container rm eclat-service
 docker rmi eclat
 
 # Step 3
-echo "Building a new image and running a container"
-docker build -t eclat:latest . # Image creation
-docker run -d -p 8881:8881 --name eclat-service eclat # Running created image in a container
+# echo "Building a new image and running a container"
+# docker build -t eclat:latest . # Image creation
+# docker run -d -p 8881:8881 --name eclat-service eclat # Running created image in a container
+
+#Step 3 updated by docker-compose
+docker-compose up
